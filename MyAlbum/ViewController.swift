215292c0765450d15c3b59d7ae3bbf582362acc2
littlemenu <jjhisking@gmail.com>
2019-08-09 18:36:54 +0900

    Initial Commit

---
+++ MyAlbum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by 정재훈 on 09/08/2019.
+//  Copyright © 2019 정재훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
